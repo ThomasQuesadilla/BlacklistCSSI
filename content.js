3933596c8837bbcb7e169abdc9dc2f0b359c45bc
gingerails <39510663+gingerails@users.noreply.github.com>
2018-07-23 15:27:35 -0400

    Update content.js
    Runs censor as a function on page load, and runs through 5 second intervals (for websites that pre-load, such as Facebook)

--- content.js
+++ content.js
@@ -1,31 +1,33 @@
-var elements = document.getElementsByTagName('*');
-//var blurImage = new Image(100, 200);
-//blurImage.src = 'bg_black_blur.png';
-//<img src="http://gamesites.nintendo.com.au/lego-city-undercover/bg_black_blur.png">
+function censor() {
+  var elements = document.getElementsByTagName('*');
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+  for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+      for (var j = 0; j < element.childNodes.length; j++) {
+          var node = element.childNodes[j];
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
+          if (node.nodeType === 3) {
+              var text = node.nodeValue;
 
-            if (text.match(/marissa/gi) || (text.match(/trump/gi))) {
-              var replace = '<span style="background-color: black; color: black;">';
-              for (var i = 0; i < text.length; i++) {
-                replace += 'X';
+              if (text.match(/marissa/gi) || (text.match(/trump/gi))) {
+                var replace = '<span style="background-color: black; color: black;">';
+                for (var i = 0; i < text.length; i++) {
+                  replace += 'X';
+                }
+                replace +='</span>';
+
+                var newNode = document.createElement('span');
+                newNode.innerHTML = replace;
+
+                element.replaceChild(newNode, element.childNodes[j]);
+
+
               }
-              replace +='</span>';
-              
-              var newNode = document.createElement('span');
-              newNode.innerHTML = replace;
-              
-              element.replaceChild(newNode, element.childNodes[j]);
-
-                
-            }
-        }
-    }
+          }
+      }
+  }
 }
+
+censor();
+setInterval(censor, 5000);
